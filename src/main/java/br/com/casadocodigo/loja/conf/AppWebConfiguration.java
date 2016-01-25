57528987da52840c159424ca97f2b671f23283ad
Wellington <wellington.nogueira@gmail.com>
2016-01-25 21:23:07 -0200

    fisrt commit

---
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses=HomeController.class)
+public class AppWebConfiguration {
+	
+	@Bean
+	private InternalResourceViewResolver internalResourceViewController() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+
+}
