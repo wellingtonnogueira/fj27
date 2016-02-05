10d8b48fbfdd2634335ca08aabb5ce14d0ec0571
wellington.nogueira@gmail.com <wellington.nogueira@gmail.com>
2016-02-04 22:56:25 -0200

    Exercise 11.6

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,25 +1,58 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.daos.UserDAO;
 
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDAO userDAO;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/products/form").hasRole("ADMIN")
-		.antMatchers("/shopping/**").hasRole("USER")
+		.antMatchers("/shopping/**").hasRole("COMPRADOR")
 		.antMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
 		.antMatchers("/products/**").permitAll()
+		.antMatchers("/resources/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin();
 		
-		
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDAO)
+			.passwordEncoder(new BCryptPasswordEncoder());
+//		AuthenticationProvider authenticationProvider = new AuthenticationProvider() {
+//			
+//			@Override
+//			public boolean supports(Class<?> authentication) {
+//				// TODO Auto-generated method stub
+//				return false;
+//			}
+//			
+//			@Override
+//			public Authentication authenticate(Authentication authentication)
+//					throws AuthenticationException {
+//				// TODO Auto-generated method stub
+//				return null;
+//			}
+//		};
+//		auth.authenticationProvider(authenticationProvider);
+	}
+	
 }
