4545df21f56555f916e34689b87734ba5fc81a85
wellington.nogueira@gmail.com <wellington.nogueira@gmail.com>
2016-02-10 22:50:05 -0200

    Exercise 14.4

--- src/test/java/br/com/casadocodigo/loja/daos/ProductDAOTest.java
+++ src/test/java/br/com/casadocodigo/loja/daos/ProductDAOTest.java
@@ -9,16 +9,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.com.casadocodigo.loja.builders.ProductsBuilder;
+import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
 import br.com.casadocodigo.loja.conf.JPAConfiguration;
 import br.com.casadocodigo.loja.models.BookType;
 import br.com.casadocodigo.loja.models.Product;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={ProductDAO.class,JPAConfiguration.class})
+@ContextConfiguration(classes={ProductDAO.class,JPAConfiguration.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
 public class ProductDAOTest {
 
 	@Autowired
